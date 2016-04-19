2dce21ee3f89e5c3c840753ff73010fe70076919
Zare Red <ZareRed@MacBook-Pro-de-Laura.local>
2016-04-19 20:27:03 +0100

    paso 3 y 4

---
+++ src/ut5recuperacionzamarareyes/Calculadora.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut5recuperacionzamarareyes;
+
+/**
+ *
+ * @author ZareRed
+ */
+public class Calculadora {
+    
+    private String Sumar;
+    private String Restar;
+    private String Multiplicar;
+    private String Dividir;
+
+    /**
+     * @return the Sumar
+     */
+    public String getSumar() {
+        return Sumar;
+    }
+
+    /**
+     * @param Sumar the Sumar to set
+     */
+    public void setSumar(String Sumar) {
+        this.Sumar = Sumar;
+    }
+
+    /**
+     * @return the Restar
+     */
+    public String getRestar() {
+        return Restar;
+    }
+
+    /**
+     * @param Restar the Restar to set
+     */
+    public void setRestar(String Restar) {
+        this.Restar = Restar;
+    }
+
+    /**
+     * @return the Multiplicar
+     */
+    public String getMultiplicar() {
+        return Multiplicar;
+    }
+
+    /**
+     * @param Multiplicar the Multiplicar to set
+     */
+    public void setMultiplicar(String Multiplicar) {
+        this.Multiplicar = Multiplicar;
+    }
+
+    /**
+     * @return the Dividir
+     */
+    public String getDividir() {
+        return Dividir;
+    }
+
+    /**
+     * @param Dividir the Dividir to set
+     */
+    public void setDividir(String Dividir) {
+        this.Dividir = Dividir;
+    }
+    
+}
